0f725b23f5217918062af32c7304a781d5ad967f
ashish Rajpoot <ashishrajput142@gmail.com>
2020-12-03 17:37:05 +0530

    add connection timeout

--- SeleniumTestNGjankins/src/test/java/com/ecom/tests/SeleniumTestNGjankins/ApplicationTest.java
+++ SeleniumTestNGjankins/src/test/java/com/ecom/tests/SeleniumTestNGjankins/ApplicationTest.java
@@ -1,5 +1,7 @@
 package com.ecom.tests.SeleniumTestNGjankins;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
@@ -7,6 +9,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import okhttp3.OkHttpClient;
 
 public class ApplicationTest {
 	
@@ -17,6 +21,7 @@ public class ApplicationTest {
   @Test
   public void testEasy() {
 	  System.setProperty("webdriver.chrome.driver", driverPath);
+	  new OkHttpClient().newBuilder().connectTimeout(100, TimeUnit.SECONDS).build();
 	  driver = new ChromeDriver();
 	  driver.get(siteUrl);
 	  soft.assertEquals("FB Login", driver.getTitle());
